32b31c945d90e16a16d10d08f1ac9e9f347ee67c
Amber <ambermcknight54@yahoo.com>
2026-04-27 11:41:36 -0500

    finished lesson 4-8

--- lesson-04-js-objects/level-08/nested.js
+++ lesson-04-js-objects/level-08/nested.js
@@ -1,5 +1,8 @@
 // TODO: Default-export an object with nested properties.
 // Example: export default { theme: { color: 'blue' } }
 export default {
+  student: {
+    name: 'Ava',
+    grade: 10
   /* TODO: nested structure */
 };
