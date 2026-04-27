e01e37b6cba631ff37718d70e1a2a9d623d60ad6
Amber <ambermcknight54@yahoo.com>
2026-04-27 11:59:13 -0500

    lesson 4-11

--- lesson-04-js-objects/level-11/object.js
+++ lesson-04-js-objects/level-11/object.js
@@ -1,6 +1,17 @@
 // TODO: Build an object from a written specification and export it as default.
 // Example spec: book with title, author, pages, and a summary() method assigned from a named function.
 const book = {
-  /* TODO: implement spec */
+  title: "The Great Gatsby",
+  author: "F. Scott Fitzgerald",
+  pages: 180,
 };
+// 2. Define the named function for the method
+function summary() {
+  return this.title + " by " + this.author;
+}
+
+// 3. Attach the function as a method to the object
+book.summary = summary;
+/* TODO: implement spec */
+
 export default book;
