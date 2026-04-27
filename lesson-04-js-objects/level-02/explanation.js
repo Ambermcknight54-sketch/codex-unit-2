2420048b80da3f1db136b2dacc6030a1967d1fcb
Amber <ambermcknight54@yahoo.com>
2026-04-27 11:27:44 -0500

    changes made to js

--- lesson-04-js-objects/level-02/explanation.js
+++ lesson-04-js-objects/level-02/explanation.js
@@ -1,3 +1,7 @@
 // TODO: Default-export a short explanation string mentioning "key" and "value".
 export default "TODO: Objects are collections of key/value pairs.";
-export default "Objects are collections of data stored as key and value pairs.";
+
+export default [
+  "Objects hold named keys mapping to values; primitives hold a single value.",
+  "Arrays are ordered lists accessed by index; objects use named keys for lookup.",
+];
