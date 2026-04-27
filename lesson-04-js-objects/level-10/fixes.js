aee7621fbd13f332339ff230b9ec2714fb92e615
Amber <ambermcknight54@yahoo.com>
2026-04-27 11:48:46 -0500

    finished lesson 4-10

--- lesson-04-js-objects/level-10/fixes.js
+++ lesson-04-js-objects/level-10/fixes.js
@@ -4,10 +4,13 @@
 
 // Buggy starter (anonymous function assigned):
 const user = { name: "Ava" };
-user.say = ???;
-
-export default user;
-
-function () {
+// 1. Define the function with a name
+function say() {
   return "hello " + user.name;
-};
+}
+
+// 2. Attach the named function to the object
+user.say = say;
+
+// 3. Export the fixed object
+export default user;
