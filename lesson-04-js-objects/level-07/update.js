919dac7fd9a112a3de3e3b0c8577b6fffb6e0969
Amber <ambermcknight54@yahoo.com>
2026-04-27 11:39:17 -0500

    finished lesson 4-7

--- lesson-04-js-objects/level-07/update.js
+++ lesson-04-js-objects/level-07/update.js
@@ -2,3 +2,11 @@
 const obj = { age: 0 };
 // TODO: update obj.age
 export default obj;
+// update.js
+const obj = { age: 0 };
+
+// Update the existing property
+obj.age = 1;
+
+// Export the final updated object
+export default obj;
