929d768a74b4b2de875065a5526165329a0ae09a
Amber <ambermcknight54@yahoo.com>
2026-04-27 11:33:31 -0500

    finished lesson 4-4

--- lesson-04-js-objects/level-04/example.js
+++ lesson-04-js-objects/level-04/example.js
@@ -1,5 +1,7 @@
 // TODO: Default-export an object literal using literal syntax.
 // Example: export default { a: 1, b: "x" };
 export default {
-  /* TODO: add keys and values */
+  name: "Ava",
+  isActive: true,
+  score: 100,
 };
