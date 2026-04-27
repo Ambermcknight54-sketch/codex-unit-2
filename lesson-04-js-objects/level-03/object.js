69e0bc276240f5a200da79a289f32a4305ed821a
Amber <ambermcknight54@yahoo.com>
2026-04-27 11:31:39 -0500

    finished lesson 4-3

--- lesson-04-js-objects/level-03/object.js
+++ lesson-04-js-objects/level-03/object.js
@@ -1,6 +1,8 @@
 // TODO: Declare a simple object and export it as the default export.
 // Example: const user = { name: 'Ava' }; export default user;
+// object.js
 const studentObject = {
-  /* TODO: add properties */
+  name: "Ava",
+  grade: 10,
 };
 export default studentObject;
