c1acbf61051802d3f1376579cf1f7952e25a81c2
Amber <ambermcknight54@yahoo.com>
2026-04-27 11:35:40 -0500

    finished lesson 4-5

--- lesson-04-js-objects/level-05/access.js
+++ lesson-04-js-objects/level-05/access.js
@@ -1,3 +1,11 @@
 // TODO: Demonstrate dot-notation property access and default-export the result/example.
 // Example: const user = { name: 'Ava' }; export default user.name;
 export default null;
+// access.js
+const student = {
+  name: 'Ava',
+  level: 5
+};
+
+// Access the 'name' property using dot notation
+export default student.name;
