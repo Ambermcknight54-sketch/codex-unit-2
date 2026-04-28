32c25bd3914788592fb93cd37d7443393a637238
Ambermcknight54-sketch <Ambermcknight54@yahoo.com>
2026-04-27 20:04:12 -0500

    finished level 5

--- lesson-05-terminal-commands/level-05/answer.js
+++ lesson-05-terminal-commands/level-05/answer.js
@@ -1,3 +1,3 @@
 // Level 05 — Parent directory (..)
 // TODO: Use `cd ..` and/or `../` to reference sibling files; describe the steps here.
-export default "";
+export default "I used 'cd ..' to move to the parent directory and ../ to reference a sibling. The ../ simply tells the terminal: ../ 'Go up one level from where I am right now, then look for this folder or file.'";
