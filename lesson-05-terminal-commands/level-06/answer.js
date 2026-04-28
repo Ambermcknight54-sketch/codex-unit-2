dce5d7512620adfeda53f7e48a2df0fc04ffc7b4
Ambermcknight54-sketch <Ambermcknight54@yahoo.com>
2026-04-27 20:54:06 -0500

    level 6 edited

--- lesson-05-terminal-commands/level-06/answer.js
+++ lesson-05-terminal-commands/level-06/answer.js
@@ -2,3 +2,4 @@
 // TODO: Run `node hello.js` and write a short note here mentioning `node`.
 // Note: Use `node` command to execute JavaScript files in the terminal.
 // answer.js that mentions `node`, node hello.js.
+export default "I used the node to run hello.js";
