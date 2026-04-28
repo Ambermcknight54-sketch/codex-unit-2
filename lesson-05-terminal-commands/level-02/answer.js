3b343cc8a96cbac721058a74620f2043b538f28c
Ambermcknight54-sketch <Ambermcknight54@yahoo.com>
2026-04-27 19:26:55 -0500

    finished level 2

--- lesson-05-terminal-commands/level-02/answer.js
+++ lesson-05-terminal-commands/level-02/answer.js
@@ -1,3 +1,3 @@
 // Level 02 — List files with ls
 // TODO: Run `ls` in the terminal and write a short note here about what you saw.
-export default "";
+export default ""I ran ls and saw: index.html and a css folder";
