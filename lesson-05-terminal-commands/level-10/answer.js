47db1c218e58e28323a0ca79c10eee85df15a23d
Ambermcknight54-sketch <Ambermcknight54@yahoo.com>
2026-04-27 21:30:43 -0500

    level 10 finished

--- lesson-05-terminal-commands/level-10/answer.js
+++ lesson-05-terminal-commands/level-10/answer.js
@@ -1,3 +1,4 @@
 // Level 10 — Challenge: Multi-step script run
 // TODO: Export an object like { steps: ["node ./folder/script.js"], notes: "moved file using VS Code" }
-export default { steps: [], notes: "" };
+export default { steps: ["node ./folder-name/script.js", "clear", "cd .."], "I moved the file using the VS Code file explorer and ran it from the terminal" }
+
