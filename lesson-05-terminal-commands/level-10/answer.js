c577fd589f4b915eb3797884df6ed169fbdc7d0b
Ambermcknight54-sketch <Ambermcknight54@yahoo.com>
2026-04-27 21:32:01 -0500

    made changes moved lone four up

--- lesson-05-terminal-commands/level-10/answer.js
+++ lesson-05-terminal-commands/level-10/answer.js
@@ -1,4 +1,3 @@
 // Level 10 — Challenge: Multi-step script run
 // TODO: Export an object like { steps: ["node ./folder/script.js"], notes: "moved file using VS Code" }
 export default { steps: ["node ./folder-name/script.js", "clear", "cd .."], "I moved the file using the VS Code file explorer and ran it from the terminal" }
-
