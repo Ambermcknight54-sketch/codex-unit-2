4ac9db18b6192646ac44409de17929eed21d6457
Amber <ambermcknight54@yahoo.com>
2026-04-29 09:53:37 -0500

    level 4

--- lesson-06-js-syntax/level-04-booleans-variables/script.js
+++ lesson-06-js-syntax/level-04-booleans-variables/script.js
@@ -3,5 +3,16 @@
 
 // Example:
 // let isLoggedIn = false;
-
+let hasSubscription = true;
+let canEditProfile = true;
+let shouldNotifyUser = false;
+let isAccountActive = true;
+let hasPendingOrders = false;
+let canUploadFiles = true;
+let shouldShowTutorial = false;
+let isAdmin = false;
+let hasAcceptedTerms = true;
 let isLoggedIn = false;
+
+// Export one of the variables as the default
+export default isLoggedIn;
