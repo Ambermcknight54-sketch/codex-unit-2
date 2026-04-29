83d14451f3bfec365097d55b8eae4de7e5c6f7a1
Amber <ambermcknight54@yahoo.com>
2026-04-29 11:09:12 -0500

    level 14 finished

--- lesson-06-js-syntax/level-14-debugging-loops/script.js
+++ lesson-06-js-syntax/level-14-debugging-loops/script.js
@@ -2,51 +2,52 @@
 // TODO: Repair the 10 broken `for` loop snippets below so each parses and prints the intended sequence.
 // Each snippet is intentionally broken; students should fix one at a time.
 
-// BROKEN: Should display 0 to 4.
-for let i = 0; i < 5; i++ { 
-  console.log(i); 
-}
-
-// BROKEN: Should display 0 to 5.
-for (let i = 1; i <= 5 i++) { 
-  console.log(i); 
-}
-
-// BROKEN: Should display 0 to 3.
-for (i = 0; i < 4; i++) 
+// 1. Should display 0 to 4.
+for (let i = 0; i < 5; i++) {
   console.log(i);
-
-// BROKEN: Should display 5 to 1.
-for (let i = 5; i > 0; i--) { 
-  console.log(i)
-
-// BROKEN: Should display even numbers 0 to 4.
-for (let i = 0; i  4; i = i + 2) { 
-  console.log(i); 
 }
 
-// BROKEN: Should display 3 to 7.
-for (let i = 3; i < =7; i++) { 
-  console.log(i); 
+// 2. Should display 0 to 5.
+for (let i = 0; i <= 5; i++) {
+  console.log(i);
 }
 
-// BROKEN: Should display 0 to 2.
-for (let i = 0; i < 3; i++) 
-  console.log(i); 
+// 3. Should display 0 to 3.
+for (let i = 0; i <= 3; i++) {
+  console.log(i);
 }
 
-// BROKEN: Should display 10 to 2 even numbers.
-for (let i 10; i > 0; i -= 2) { 
-  console.log(i); 
+// 4. Should display 5 to 1.
+for (let i = 5; i > 0; i--) {
+  console.log(i);
 }
 
-// BROKEN: Should display 0.
-for (let i = 0; i < 1; i); { 
-  console.log(i); 
+// 5. Should display even numbers 0 to 4.
+for (let i = 0; i <= 4; i += 2) {
+  console.log(i);
 }
 
-// BROKEN: Should display 0 to 9.
-for (let i = 0; i < 10; i=i) { 
-  console.log(i); 
+// 6. Should display 3 to 7.
+for (let i = 3; i <= 7; i++) {
+  console.log(i);
 }
 
+// 7. Should display 0 to 2.
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
+
+// 8. Should display 10 to 2 even numbers.
+for (let i = 10; i >= 2; i -= 2) {
+  console.log(i);
+}
+
+// 9. Should display 0.
+for (let i = 0; i < 1; i++) {
+  console.log(i);
+}
+
+// 10. Should display 0 to 9.
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
