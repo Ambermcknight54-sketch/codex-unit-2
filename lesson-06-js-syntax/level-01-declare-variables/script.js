77d82100477ee8c693b144bbf3502ec20c4553b4
Amber <ambermcknight54@yahoo.com>
2026-04-29 09:30:15 -0500

    level 1 finished

--- lesson-06-js-syntax/level-01-declare-variables/script.js
+++ lesson-06-js-syntax/level-01-declare-variables/script.js
@@ -1,6 +1,21 @@
 // Level 01 — Declare 10 variables (use `let`)
 // TODO: Declare ten `let` variables with NO initial value, one per line.
+// Level 01 — Declare 10 variables (use `let`)
+export default userFirstName;
+// Export one variable as the default
+let userFirstName;
+let userLastName;
+let userEmailAddress;
+let userPhoneNumber;
+let userAccountStatus;
+let userDateOfBirth;
+let userShippingAddress;
+let userMembershipLevel;
+let userTotalPoints;
+let userPreferredLanguage;
 
+// Export one variable as the default
+export default userFirstName;
 // Example:
 // let userName;
 
