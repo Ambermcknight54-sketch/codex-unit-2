6d742601009ca92e135902bc1368bc6d470b1001
Amber <ambermcknight54@yahoo.com>
2026-04-29 09:50:09 -0500

    level 3 edited finished

--- lesson-06-js-syntax/level-03-practice-variables/script.js
+++ lesson-06-js-syntax/level-03-practice-variables/script.js
@@ -1,12 +1,17 @@
 // Level 03 — Assign values and debug string initializations
 // TODO: Part A: assign values to five declared variables (on separate lines).
 
+let firstName;
+let lastName;
+let userAge;
+let city;
+let country;
+
 firstName = "Ava";
 lastName = "Smith";
 userAge = 28;
 city = "New York";
 country = "USA";
-
 // TODO: Part B: fix five faulty string initializations so they parse correctly.
 
 let userName = "noName";
