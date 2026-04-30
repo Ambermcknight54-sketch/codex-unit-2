610664c5209b248b4a0ed4cfde6b96ddf8f45518
Amber <ambermcknight54@yahoo.com>
2026-04-29 20:06:29 -0500

    finished level 2

--- lesson-07-browser-objects/level-02-getElementById/script.js
+++ lesson-07-browser-objects/level-02-getElementById/script.js
@@ -5,3 +5,12 @@
 // Example scaffold (fill in the TODOs):
 // const el = document.getElementById('sample');
 // console.log(el);
+1. Select the element with document.getElementById('sample')
+const el = document.getElementById('sample');
+
+// 2. Log properties: id, classList, innerText, innerHTML, attributes
+console.log("ID:", el.id);
+console.log("Class List:", el.classList);
+console.log("Inner Text:", el.innerText);
+console.log("Inner HTML:", el.innerHTML);
+console.log("Attributes:", el.attributes);
