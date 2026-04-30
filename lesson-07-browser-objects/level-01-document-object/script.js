8e16f67c7a6179fdd6f44e05aa2f4bacdadeec52
Amber <ambermcknight54@yahoo.com>
2026-04-29 20:03:15 -0500

    level 1 finished

--- lesson-07-browser-objects/level-01-document-object/script.js
+++ lesson-07-browser-objects/level-01-document-object/script.js
@@ -1,1 +1,7 @@
 debugger;
+console.log the title, URL, scripts, and styleSheets of the document
+
+console.log(document.title);
+console.log(document.URL);
+console.log(document.scripts);
+console.log(document.styleSheets);
