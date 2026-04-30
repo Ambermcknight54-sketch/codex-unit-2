63fc4cc6e31e05d139d289a17a530d630f1ad929
Amber <ambermcknight54@yahoo.com>
2026-04-29 20:16:18 -0500

    level 4 finished

--- lesson-07-browser-objects/level-04-innerHTML/script.js
+++ lesson-07-browser-objects/level-04-innerHTML/script.js
@@ -7,3 +7,16 @@
 // console.log('before innerHTML:', el && el.innerHTML);
 // // TODO: el.innerHTML = '<strong>New content</strong>';
 // console.log('after innerHTML:', el && el.innerHTML);
+1. Select the element
+const el = document.getElementById('content');
+
+// 2. Log previous innerHTML (this will include the <strong> tags)
+console.log('before innerHTML:', el && el.innerHTML);
+
+// 3. Set a small markup string
+if (el) {
+    el.innerHTML = 'Updated content with <em>emphasized</em> text!';
+}
+
+// 4. Log the after value
+console.log('after innerHTML:', el && el.innerHTML);
