2196001c64519235cc1abd7ab51ddf94d9b985a7
Amber <ambermcknight54@yahoo.com>
2026-05-05 13:19:39 -0500

    level 6 finished

--- lesson-08-rest-api/level-06/script.js
+++ lesson-08-rest-api/level-06/script.js
@@ -7,6 +7,17 @@ function handleSubmit(e) {
   e.preventDefault();
   // TODO: build a `data` object from form.elements using indexes (e.g. form.elements[0].value)
   // Example display: out.innerText = JSON.stringify(data, null, 2);
+// Collect values using indices:
+  // form.elements is the first input (Username)
+  // form.elements is the second input (Email)
+  const data = {
+    username: form.elements.value,
+    email: form.elements.value
+  };
+
+  // Display the object in the <pre> tag
+  out.innerText = JSON.stringify(data, null, 2);
+}
 }
 
 if (form) form.onsubmit = handleSubmit;
