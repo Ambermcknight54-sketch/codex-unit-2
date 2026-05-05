062cc6a051a161cd6afba0315a6d99059fac7d76
Amber <ambermcknight54@yahoo.com>
2026-05-05 13:04:58 -0500

    level 1 finished

--- lesson-08-rest-api/level-01/script.js
+++ lesson-08-rest-api/level-01/script.js
@@ -2,3 +2,6 @@
 // TODO: Select a target element and set its innerText to explain REST API.
 const result = document.getElementById("result");
 // Example: result.innerText = '...'; // fill this in during the exercise
+// Set the innerText to explain REST API
+result.innerText =
+  "A REST API (Representational State Transfer) is an architectural style that allows different software applications to communicate with each other over the internet using standard HTTP methods like GET, POST, PUT, and DELETE.";
