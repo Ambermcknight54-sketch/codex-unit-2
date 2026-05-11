d55449de82e13b8f50829448496337ee27130a20
Amber <ambermcknight54@yahoo.com>
2026-05-11 18:05:52 -0500

    level 12 finished

--- lesson-07-browser-objects/level-12-radio-checkbox/script.js
+++ lesson-07-browser-objects/level-12-radio-checkbox/script.js
@@ -3,6 +3,33 @@
 
 function handleSubmit(event) {
   // event.preventDefault();
+  event.preventDefault();
+
+  // 2. Reference the form
+  const form = event.target;
+
+  // 3. Read the Radio Group value
+  // This automatically finds the one radio button that is "checked"
+  const color = form.elements.color.value;
+
+  // 4. Read the Checkbox state
+  // We use .checked because we want a true/false boolean, not the string "on"
+  const subscribe = form.elements.subscribe.checked;
+
+  // 5. Display the results
+  const display = document.getElementById('display-area');
+  if (display) {
+    display.innerText = `Color: ${color || 'None'}. Subscribed: ${subscribe}`;
+  }
+
+  console.log("Selection Data:", { color, subscribe });
+}
+
+// 6. Assign the handler
+const form = document.getElementById('sample-form');
+if (form) {
+  form.onsubmit = handleSubmit;
+}
   // const form = event.target;
   // TODO: const color = form.elements.color.value;
   // TODO: const subscribe = form.elements.subscribe.checked;
