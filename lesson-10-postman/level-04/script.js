5a3a26224ad7181bd695d94a72accc2b60e1ac5c
Amber <ambermcknight54@yahoo.com>
2026-05-11 19:43:56 -0500

    level 4 postman finished

--- lesson-10-postman/level-04/script.js
+++ lesson-10-postman/level-04/script.js
@@ -1,4 +1,8 @@
 // TODO: Starter for Level 04: POST request basics.
 
 // Students should use Postman to send a POST; this file is a placeholder
-console.log("TODO: log one documentation fact here");
+// Fact learned: The login endpoint requires both a 'username' and 'password'.
+
+console.log(
+  "Fact: A successful POST to the login endpoint returns an 'accessToken' which is used for authorized requests.",
+);
