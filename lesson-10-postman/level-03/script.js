717206bfe0cdeac13a521c7f8a825f65422c3dc6
Amber <ambermcknight54@yahoo.com>
2026-05-11 19:39:16 -0500

    level 3-postman finished

--- lesson-10-postman/level-03/script.js
+++ lesson-10-postman/level-03/script.js
@@ -1,4 +1,6 @@
 // TODO: Starter for Level 03: experiment with query parameters.
 
 // Students may optionally replicate the request in code and log differences.
-console.log("TODO: log one documentation fact here");
+console.log(
+  "Fact: According to the docs, the maximum value for the 'limit' parameter is 50.",
+);
