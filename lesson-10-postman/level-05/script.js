af07fed4646a8664f3a7ef9c45e698ae13433aa2
Amber <ambermcknight54@yahoo.com>
2026-05-11 19:49:26 -0500

    level 5-10 finished

--- lesson-10-postman/level-05/script.js
+++ lesson-10-postman/level-05/script.js
@@ -1,4 +1,10 @@
 // TODO: Starter for Level 05: POST with JSON payloads.
 
 // Placeholder: students should set Content-Type and send JSON in Postman.
-console.log("TODO: log one documentation fact here");
+console.log(
+  "Fact learned: The login response includes the user's email, gender, and profile image.",
+);
+
+console.log(
+  "Fact: A successful login provides an 'expiresInMins' value, which defaults to 60 minutes if not specified.",
+);
