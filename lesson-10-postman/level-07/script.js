2c9fcb206f2dd2ae8e02a4a7b8dad7f3ec46edf4
Amber <ambermcknight54@yahoo.com>
2026-05-11 21:24:27 -0500

    level 7 complete

--- lesson-10-postman/level-07/script.js
+++ lesson-10-postman/level-07/script.js
@@ -2,3 +2,24 @@
 
 // Students should construct a JSON payload and send it to their chosen API.
 console.log("TODO: log one documentation fact here");
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  body: JSON.stringify({
+    title: "My First Post",
+    body: "Learning how to send JSON payloads is awesome.",
+    userId: 1,
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Success! Server returned:", data);
+
+    // Fact from documentation: JSONPlaceholder doesn't actually save the data.
+    console.log(
+      "Fact: The server will return a 201 status code and an ID of 101 to simulate creation.",
+    );
+  })
+  .catch((error) => console.error("Error posting data:", error));
