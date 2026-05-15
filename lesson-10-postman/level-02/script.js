c6ddaad92a91b5a3c10782593357b01787168a26
Amber <ambermcknight54@yahoo.com>
2026-05-14 22:10:57 -0500

    made chages in level 2 post man

--- lesson-10-postman/level-02/script.js
+++ lesson-10-postman/level-02/script.js
@@ -2,5 +2,6 @@
 // Example: Console.log one fact learned from the API docs.
 
 // TODO: Replace this placeholder with a short note from the docs.
-console.log("When you send a GET request to https://dummyjson.com/users, you are asking the server to "Get" the list of user resources.")
-    ;
+console.log(
+  "When you send a GET request to https://dummyjson.com/users, you are asking the server to Get the list of user resources.",
+);
