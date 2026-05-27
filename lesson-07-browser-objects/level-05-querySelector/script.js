cad10b18188a9bf0183e04fe076a366d86561f07
Amber <ambermcknight54@yahoo.com>
2026-05-27 18:28:43 -0500

    level 5-7 complete

--- lesson-07-browser-objects/level-05-querySelector/script.js
+++ lesson-07-browser-objects/level-05-querySelector/script.js
@@ -1,1 +1,10 @@
 // TODO: Use querySelector('#content') to select element and update innerHTML to explain querySelector vs getElementById
+// 1. Target the element using a modern CSS selector string
+const element = document.querySelector("#content");
+console.log("before:", element.innerHTML);
+element.innerHTML = `
+    <output style="display: block; color: #333333; lineHeight: 1.5;">
+        querySelector allows targeting elements with any flexible CSS style selector, 
+        whereas getElementById is limited strictly to matching a unique ID attribute identifier.
+    </output>
+`;
