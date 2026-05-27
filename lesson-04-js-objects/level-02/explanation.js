5fe50254734793145820ed0269dd120e5bd27c62
Amber <ambermcknight54@yahoo.com>
2026-05-27 17:11:11 -0500

    completed part 2 level 2-4

--- lesson-04-js-objects/level-02/explanation.js
+++ lesson-04-js-objects/level-02/explanation.js
@@ -2,3 +2,8 @@
 export default "TODO: Objects are collections of key/value pairs.";
 const objectExplanation = "An object is a collection of data stored as key and value pairs.";
 export default objectExplanation;
+const objectComparisons = [
+    "Primitives store a single data value while objects store multiple named values",
+    "Arrays arrange data elements by index numbers whereas objects look up data values using keys"
+];
+export default objectComparisons;
