e64459f1939ef70f05c8cad69982fd9b1493cc18
Amber <ambermcknight54@yahoo.com>
2026-05-27 17:07:37 -0500

    level 1-4

--- lesson-04-js-objects/level-01/examples.js
+++ lesson-04-js-objects/level-01/examples.js
@@ -1,3 +1,5 @@
 // TODO: Default-export an array of two short example strings.
 // Example: export default ["{ a: 1 }", "{ name: 'Ava' }"]
 export default ["TODO example 1", "TODO example 2"];
+const objectDescription = "Objects are collections of key value pairs accessed by name";
+export default objectDescription;
