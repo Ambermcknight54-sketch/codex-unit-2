6f6822b3c84bfd1fdff3b6e9e245c4d6cee4eef2
Amber <ambermcknight54@yahoo.com>
2026-05-27 17:09:21 -0500

    level 2-4 finished

--- lesson-04-js-objects/level-02/explanation.js
+++ lesson-04-js-objects/level-02/explanation.js
@@ -1,2 +1,4 @@
 // TODO: Default-export a short explanation string mentioning "key" and "value".
 export default "TODO: Objects are collections of key/value pairs.";
+const objectExplanation = "An object is a collection of data stored as key and value pairs.";
+export default objectExplanation;
