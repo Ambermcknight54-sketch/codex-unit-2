d94fb286ea8bf252c2034fc0cbe1138a03a452ea
Amber <ambermcknight54@yahoo.com>
2026-05-27 17:17:50 -0500

    added the "c" to const

--- lesson-04-js-objects/level-05/access.js
+++ lesson-04-js-objects/level-05/access.js
@@ -1,11 +1,9 @@
 // TODO: Demonstrate dot-notation property access and default-export the result/example.
 // Example: const user = { name: 'Ava' }; export default user.name;
-onst targetUser = {
+const targetUser = {
   username: "eveHolt",
-  role: "admin"
+  role: "admin",
 };
 
 const extractedUsername = targetUser.username;
 export default extractedUsername;
-
-
