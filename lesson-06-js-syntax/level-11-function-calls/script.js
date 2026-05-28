55e37d7a21a43f6805ec1cb0690451a41c20753f
Amber <ambermcknight54@yahoo.com>
2026-05-28 18:25:13 -0500

    level 11-6

--- lesson-06-js-syntax/level-11-function-calls/script.js
+++ lesson-06-js-syntax/level-11-function-calls/script.js
@@ -4,5 +4,15 @@
 export function reportScore(playerName, score) {
   return playerName + " scored " + score + " points";
 }
+const avaScoreReport = reportScore("Ava", 0); // Keeping your initial starter line
+const jonScoreReport = reportScore("Jon", 85);
+const mayaScoreReport = reportScore("Maya", 92);
+const leoScoreReport = reportScore("Leo", 74);
+const elenaScoreReport = reportScore("Elena", 110);
+const chloeScoreReport = reportScore("Chloe", 95);
+const marcusScoreReport = reportScore("Marcus", 68);
+const sashaScoreReport = reportScore("Sasha", 105);
+const devonScoreReport = reportScore("Devon", 81);
+const fionaScoreReport = reportScore("Fiona", 89);
 
 const avaScoreReport = reportScore("Ava", 0);
