d6868ee496749ada95c0be7c215d0ef388d993d3
Amber <ambermcknight54@yahoo.com>
2026-05-27 19:04:41 -0500

    level7-7 finished

--- lesson-07-browser-objects/level-07-submit-handler/script.js
+++ lesson-07-browser-objects/level-07-submit-handler/script.js
@@ -4,3 +4,13 @@
 // const form = document.getElementById('sample-form');
 // function handleSubmit(event) { console.log('onsubmit handler'); }
 // form.onsubmit = handleSubmit;
+const form = document.getElementById("sample-form");
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(
+    "onsubmit fires immediately when a user attempts to submit a form structure via buttons or entry events.",
+  );
+}
+
+form.onsubmit = handleSubmit;
