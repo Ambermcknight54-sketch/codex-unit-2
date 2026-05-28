66edfb609a95d3544072fa2ebbaff794893b611c
Amber <ambermcknight54@yahoo.com>
2026-05-27 21:25:03 -0500

    level 6-8 complete

--- lesson-08-rest-api/level-06/script.js
+++ lesson-08-rest-api/level-06/script.js
@@ -2,21 +2,13 @@
 // TODO: Use form.elements (by index) to collect input values into a plain object.
 const form = document.getElementById("sample-form");
 const out = document.getElementById("data");
+form.onsubmit = handleSubmit;
 
 function handleSubmit(e) {
-  e.preventDefault();
-  // TODO: build a `data` object from form.elements using indexes (e.g. form.elements[0].value)
-  // Example display: out.innerText = JSON.stringify(data, null, 2);
-  // Collect values using indices:
-  // form.elements is the first input (Username)
-  // form.elements is the second input (Email)
+  event.preventDefault();
   const data = {
     username: form.elements.value,
     email: form.elements.value,
   };
-
-  // Display the object in the <pre> tag
   out.innerText = JSON.stringify(data, null, 2);
 }
-
-if (form) form.onsubmit = handleSubmit;
