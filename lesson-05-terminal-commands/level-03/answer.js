6a10f424d355e7089b94821d5db6f7797b331aa1
Amber <ambermcknight54@yahoo.com>
2026-05-28 14:24:42 -0500

    level 3-5 done

--- lesson-05-terminal-commands/level-03/answer.js
+++ lesson-05-terminal-commands/level-03/answer.js
@@ -1,3 +1,3 @@
 // Level 03 — Change directory with cd
 // TODO: Use `cd` to move into a folder and back; describe your commands here.
-export default "";
+export default "I used 'ls' to locate a subfolder, used 'cd' to move into that directory, and then executed 'cd ..' to return to the parent workspace.";
