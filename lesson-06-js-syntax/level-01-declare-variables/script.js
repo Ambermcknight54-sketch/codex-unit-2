5d30d303d4f1699fde39d6cb1dd14d80cd1628d6
Amber <ambermcknight54@yahoo.com>
2026-05-28 15:10:25 -0500

    used consr

--- lesson-06-js-syntax/level-01-declare-variables/script.js
+++ lesson-06-js-syntax/level-01-declare-variables/script.js
@@ -5,15 +5,14 @@
 // let userName;
 
 // Add your lines below:
-let currentUserId;
-let userProfileName;
-let totalItemCount;
-let isSessionActive;
-let primaryThemeColor;
-let userEmailAddress;
-let maximumLoginAttempts;
-let accountCreationDate;
-let activeScreenIndex;
-let systemErrorMessage;
-
+const currentUserId = null;
+const userProfileName = "Guest";
+const totalItemCount = 0;
+const isSessionActive = false;
+const primaryThemeColor = "#ffffff";
+const userEmailAddress = "";
+const maximumLoginAttempts = 5;
+const accountCreationDate = "2026-01-01";
+const activeScreenIndex = 0;
+const systemErrorMessage = "None";
 export default currentUserId;
