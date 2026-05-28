3002fec40bbe43efbb916afa3f676b25d320859d
Amber <ambermcknight54@yahoo.com>
2026-05-28 14:30:29 -0500

    level 4 -5 complete

--- lesson-05-terminal-commands/level-04/answer.js
+++ lesson-05-terminal-commands/level-04/answer.js
@@ -1,3 +1,3 @@
 // Level 04 — Current directory (.)
 // TODO: Run a script using `./` and write a short note here mentioning `.` or `./`.
-export default "";
+export default "Executing 'cd .' keeps you in the current directory, and using './' tells the terminal to look for a file in this exact folder.";
