bda56d383118c5931930f91d2e7e79170c96ba62
Amber <ambermcknight54@yahoo.com>
2026-05-28 14:33:59 -0500

    level 6-5 finished

--- lesson-05-terminal-commands/level-06/answer.js
+++ lesson-05-terminal-commands/level-06/answer.js
@@ -1,3 +1,4 @@
 // Level 06 — Run Node.js files with node
 // TODO: Run `node hello.js` and write a short note here mentioning `node`.
-export default "";
+export default "I executed the script in the integrated terminal using the 'node' command to interpret the JavaScript file.";
+console.log("Hello from Node!");
