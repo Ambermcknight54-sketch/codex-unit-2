f31a51b2ad0979bc2380c06cea7b6cb48edb1d00
Amber <ambermcknight54@yahoo.com>
2026-05-28 14:57:19 -0500

    level 10-5

--- lesson-05-terminal-commands/level-10/answer.js
+++ lesson-05-terminal-commands/level-10/answer.js
@@ -1,3 +1,12 @@
 // Level 10 — Challenge: Multi-step script run
 // TODO: Export a string array with the VS Code steps and terminal commands you used.
 export default [];
+const challengeSteps = [
+  "created folder",
+  "added file",
+  "node ./folder/script.js",
+  "clear",
+  "cd .."
+];
+
+export default challengeSteps;
