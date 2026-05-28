db91c7dff62d4624947f05409b4f354ea8bf3fc3
Amber <ambermcknight54@yahoo.com>
2026-05-27 23:11:07 -0500

    removed a curly brace

--- lesson-09-public-apis/level-08/script.js
+++ lesson-09-public-apis/level-08/script.js
@@ -35,4 +35,4 @@ function handleSubmit(event) {
   // TODO: Create a `data` object with an "api-key" property (quoted when hyphenated)
   // TODO: Convert `data` into a query string and fetch jokes (or send as header if API requires)
   // TODO: Parse response and render a joke into the DOM. On invalid key, show an error message in the page
-}
+
