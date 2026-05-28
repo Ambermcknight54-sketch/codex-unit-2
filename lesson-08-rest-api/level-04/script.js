1758e9dade3e18a3b65e02c605e5745c3f9fe166
Amber <ambermcknight54@yahoo.com>
2026-05-27 20:50:45 -0500

    made a change removed curly brace

--- lesson-08-rest-api/level-04/script.js
+++ lesson-08-rest-api/level-04/script.js
@@ -6,20 +6,14 @@ form.onsubmit = handleSubmit;
 
 async function handleSubmit(event) {
   event.preventDefault();
-  // TODO: fetch and parse JSON, then set result.innerText with a short explanation
-  try {
     // 1. Fetch the data from the API
     const response = await fetch("https://api.jsoning.com/mock/public/users");
-
     // 2. Parse the body as JSON
     const data = await response.json();
-
-    // 3. Update the result with a short explanation
+     // 3. Update the result with a short explanation
     result.innerText = `Request successful! We retrieved ${data.length} user records from the API.`;
   } catch (error) {
     // Basic error handling
     result.innerText = "Failed to fetch data: " + error.message;
   }
-}
 
-form.onsubmit = handleSubmit;
