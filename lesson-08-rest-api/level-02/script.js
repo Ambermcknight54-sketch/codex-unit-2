b906197e722d4df5b984d60ca6a2d0d3e58f3e80
Amber <ambermcknight54@yahoo.com>
2026-05-27 20:25:05 -0500

    level 2-8 complete

--- lesson-08-rest-api/level-02/script.js
+++ lesson-08-rest-api/level-02/script.js
@@ -2,6 +2,7 @@
 // TODO: Get & save the form element to `form`, attach onsubmit, and call event.preventDefault().
 const form = document.getElementById("sample-form");
 const result = document.getElementById("result");
+form.onsubmit = handleSubmit;
 
 async function handleSubmit(event) {
   event.preventDefault(); // Prevents the form from refreshing the page
@@ -11,12 +12,9 @@ async function handleSubmit(event) {
       "https://jsonplaceholder.typicode.com/posts/1",
     );
     const data = await response.json();
-
-    // Update result with the data retrieved
-    result.innerText = `Success! Fetched title: "${data.title}"`;
+    result.innerText = `Success! Fetched title: "(data.title)";
   } catch (error) {
     result.innerText = "There was an error fetching the data.";
   }
 }
 
-if (form) form.onsubmit = handleSubmit;
