feb0828409eda2e151b11ca4fbaca3c3f9b080a0
Amber <ambermcknight54@yahoo.com>
2026-06-03 10:16:09 -0500

    added export default

--- lesson-04-js-objects/level-01/examples.js
+++ lesson-04-js-objects/level-01/examples.js
@@ -1,7 +1,7 @@
 // TODO: Default-export an array of two short example strings.
 // Example: export default ["{ a: 1 }", "{ name: 'Ava' }"]
-export default ["TODO example 1", "TODO example 2"];
-export const examples = [
+export default ["example: 1", "example 2"];
+export default  [
   "padding",
   "border",
   "margin",
