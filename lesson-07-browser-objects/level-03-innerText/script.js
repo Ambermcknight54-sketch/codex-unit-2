2bcc53bbb786adcff6343c4fd4ccc206daea30e8
Amber <ambermcknight54@yahoo.com>
2026-06-03 10:55:22 -0500

    added set new inner to a new descriptive string that mentions 'innerText'

--- lesson-07-browser-objects/level-03-innerText/script.js
+++ lesson-07-browser-objects/level-03-innerText/script.js
@@ -9,8 +9,7 @@ const element = document.getElementById("desc");
 console.log("before:", element.innerText);
 
 // 3. Set element.innerText to a new descriptive string that mentions 'innerText'
-element.innerText =
-  "The innerText property updates only the visible plain text content of an element.
+element.innerText = "The innerText property updates only the visible plain text content of an element.
 
 // 4. Use the debugger to inspect changes
 debugger;
