3fe778c42045ab803558386d70a27b0fb539953b
Amber <ambermcknight54@yahoo.com>
2026-06-06 10:22:51 -0500

    fixing for updates

--- lesson-08-rest-api/level-03/script.js
+++ lesson-08-rest-api/level-03/script.js
@@ -1,6 +1,7 @@
 // Level 03 starter script
 // TODO: Use async/await when calling fetch inside the submit handler.
 const form = document.getElementById("sample-form");
+form.onsubmit = handleSubmit;
 
 async function handleSubmit(event) {
   event.preventDefault();
