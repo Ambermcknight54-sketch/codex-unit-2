c3e48aa043b8e7cd9bc31c1b14986131b120aeaa
Amber <ambermcknight54@yahoo.com>
2026-06-09 18:03:23 -0500

    removed explaination

--- lesson-04-js-objects/level-02/explanation.js
+++ lesson-04-js-objects/level-02/explanation.js
@@ -1,6 +1,5 @@
 // TODO: Default-export a short explanation string mentioning "key" and "value".
-export default objectExplanation;
-const objectComparisons = [
+export default [
   "Primitives store a single data value while objects store multiple named values",
   "Arrays arrange data elements by index numbers whereas objects look up data values using keys",
 ];
