24c195ecff9e58f6a39246d442ad2ade2a7caa3d
Amber <ambermcknight54@yahoo.com>
2026-06-09 18:00:44 -0500

    removed export default

--- lesson-04-js-objects/level-01/examples.js
+++ lesson-04-js-objects/level-01/examples.js
@@ -1,10 +1,3 @@
 // TODO: Default-export an array of two short example strings.
 // Example: export default ["{ a: 1 }", "{ name: 'Ava' }"]
 export default ["example: 1", "example 2"];
-export default  [
-  "padding",
-  "border",
-  "margin",
-  "content",
-  "box-sizing",
-];
